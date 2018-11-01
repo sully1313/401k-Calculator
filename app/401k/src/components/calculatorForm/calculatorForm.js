8a4cb34fc01ec5f0a3f366bf095d87e24aef6e96
Matt Sullivan <mattsullivan@Matts-MacBook-Pro.local>
2018-10-31 22:01:05 -0700

    wip

--- app/401k/src/components/calculatorForm/calculatorForm.js
+++ app/401k/src/components/calculatorForm/calculatorForm.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import './style.js'
+
+const List = styled.li`
+  list-style: none;
+`
 
 const Form = styled.form`
-  //
+
   `
 
 const Input = styled.input`
-  //
 `
 const Submit = styled.button`
   //
@@ -16,48 +20,62 @@ class CalculatorForm extends Component {
   render () {
     return(
       <Form>
-        <label>
-          Enter your Age:
-          <Input
-            name="startingAge"
-            type="float" />
-        </label>
-        <label>
-          Enter the age you would like it stop calculating at:
-          <Input
-            name="endingAge"
-            type="float" />
-        </label>
-        <label>
-          Enter the age you would like to stop contributing to 401k:
-          <Input
-            name="endContributionAge"
-            type="float" />
-        </label>
-        <label>
-          Enter your yearly contribution amount:
-          <Input
-            name="yearlyContribution"
-            type="float" />
-        </label>
-        <label>
-          Enter the percent your company is willing to match:
-          <Input
-            name="companyMatchPercent"
-            type="float" />
-        </label>
-        <label>
-          Enter your yearly salary:
-          <Input
-            name="usersSalary"
-            type="float" />
-        </label>
-        <label>
-          Enter the percent you expect your 401k to grow at:
-          <Input
-            name="interestRateGrowth"
-            type="float" />
-        </label>
+        <List>
+          <label>
+            Enter your Age:
+            <Input
+              name="startingAge"
+              type="float" />
+          </label>
+        </List>
+        <List>
+          <label>
+            Enter the age you would like it stop calculating at:
+            <Input
+              name="endingAge"
+              type="float" />
+          </label>
+        </List>
+        <List>
+          <label>
+            Enter the age you would like to stop contributing to 401k:
+            <Input
+              name="endContributionAge"
+              type="float" />
+          </label>
+        </List>
+        <List>
+          <label>
+            Enter your yearly contribution amount:
+            <Input
+              name="yearlyContribution"
+              type="float" />
+          </label>
+        </List>
+        <List>
+          <label>
+            Enter the percent your company is willing to match:
+            <Input
+              name="companyMatchPercent"
+              type="float" />
+          </label>
+        </List>
+        <List>
+          <label>
+            Enter your yearly salary:
+            <Input
+              name="usersSalary"
+              type="float" />
+          </label>
+        </List>
+        <List>
+          <label>
+            Enter the percent you expect your 401k to grow at:
+            <Input
+              name="interestRateGrowth"
+              type="float" />
+          </label>
+        </List>
         <Submit type="submit" value="Submit">
           Submit
         </Submit>
